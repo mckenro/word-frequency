e743928a4de6900605335132147229b851ee0a34
Rob McKenzie <mckenro@gmail.com>
2017-03-24 10:47:09 -0700

    add a views page with a form and routing on the app.php page

--- app/app.php
+++ app/app.php
@@ -1,20 +1,23 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    //require_once __DIR__."/../src/Class.php";
-
-    use Symfony\Component\Debug\Debug;
-    Debug::enable();
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
-
-    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
 ));
 
     $app->get("/", function() use ($app) {
-    return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig');
 });
+
+    $app->post("/result", function() use ($app) {
+        $input_sentence = $_POST['input-sentence'];
+        $input_word = $_POST['input-word'];
+        $user_count = new RepeatCounter;
+        return $app['twig']->render('index.html.twig', array('count' => $user_count->countRepeats($input_sentence, $input_word), 'final-word' => $input_word, 'final-sentence' => $input_sentence));
+    });
+
     return $app;
  ?>
